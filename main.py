f7eb3b44c38dbb0d80761669a3c4de935de56407
s25415 <s25415@pjwstk.edu.pl>
2023-03-10 19:34:48 +0100

    Start

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import webbrowser
 import requests
 
-if __name__ == '__main__':
-    print("hello world")
-    url = input("Podaj stronę internetową: ")
-    webbrowser.open(url)
+pageurl = input("Podaj stronę internetową: ")
+dates = {20150101, 20100101, 20050101}
+for i in dates:
+    url ="http://archive.org/wayback/available?url="+pageurl+"&timestamp="+str(i)
     response = requests.get(url)
     d = response.json()
     page = d["archived_snapshots"]["closest"]["url"]
-    webbrowser.open(page)
+    webbrowser.open(page);
